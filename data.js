24232dc28e78997279909d7b276686f150738a6f
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-03-03 12:29:01 +0530

    Update data.js

--- data.js
+++ data.js
@@ -30,13 +30,22 @@ deleteAll=()=>{
         }
     })
 }
+// delete single
 
+deleteOne=(user)=>{
+ this.setState((prevState)=>{
+     return{
+         userData:prevState.userData.filter((us) => user !== us)
+     }
+ })
+}
 render(){
       return(
             <div>
                 <Users udata={this.state.userData}
                 da={this.deleteAll}
                 hasData={this.state.userData.length > 0}
+                dOne={this.deleteOne}
                 />
                 <Header hdata={this.state.headerData}/>
                 <p>MainApp Component</p>
@@ -47,9 +56,6 @@ render(){
         )
     }
 }
-
-
-
 
 
 
@@ -66,9 +72,30 @@ export default class Users extends Component{
         return(
             <div>
                 {
-                    this.props.udata.map((ud) =>  <User key={ud} userNew={ud}/>)
+                    this.props.udata.map((ud) =>  <User key={ud} userNew={ud} duser={this.props.dOne}/>)
                 }
               <button disabled={!this.props.hasData} onClick={this.props.da}>DeleteAll</button>
+            </div>
+           
+        )
+    }
+}
+
+
+
+
+import { Component } from "react";
+
+
+
+
+export default class User extends Component{
+
+    render(){
+        return(
+            <div>
+                {this.props.userNew}
+                <button onClick={()=> this.props.duser(this.props.userNew)}>Delete</button>
             </div>
            
         )
