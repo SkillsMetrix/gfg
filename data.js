01ddf9e8dfe5ae4292e9b68824f4951dff0fda64
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-03-10 10:52:45 +0530

    Update data.js

--- data.js
+++ data.js
@@ -2,15 +2,25 @@ import React, { Component } from "react";
 import axios from "axios";
 const URL = "https://jsonplaceholder.typicode.com/users";
 export default class RestApp extends Component {
+  state={
+    users:[]
+  }
   componentDidMount() {
     axios
       .get(URL)
       .then((response) => response.data)
       .then((data) => {
-        console.log(data);
+      
+        this.setState({users:data})
       });
   }
   render() {
-    return <div></div>;
+    return <div>
+     {this.state.users.map((udata)=> (
+       <div>
+         {udata.username} -- {udata.email}
+       </div>
+     ))}
+    </div>;
   }
 }
