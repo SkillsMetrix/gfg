ba91e78ff9b0cba2320240c2f8af49e4ceef3d70
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-03-23 11:37:12 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,44 +1,25 @@
-
-import React, { useState, useEffect } from 'react';
-
-function Demo2(props) {
-    const [users,setUsers] = useState([])
-    const [uname,setUname]=useState('')
-    const [email,setEmail]=useState('')
-
-    useEffect(()=>{
-        localStorage.setItem('gfg',JSON.stringify(users))
-    },[users])
+import React, { Component, useState, useEffect } from "react";
+import axios from "axios";
+ const URL = "https://jsonplaceholder.typicode.com/users";
+function RestApp (){
   
-     const addUser=(e)=>{
-      e.preventDefault()
-      setUsers([
-          ...users,{uname,email}
-      ])
-      setUname('')
-      setEmail('')
-     }
-     const deleteUser=(uname)=>{
-              setUsers(users.filter((note)=> note.uname !== uname))
-     }
-    return (
-        <div>
-            {users.map((data)=>(
-                <div key={data.uname}>
-                    {data.uname}-- {data.email}
-                    <button onClick={()=> deleteUser(data.uname)}>Delete</button>
-                </div>
-            ))}
-            <hr/>
-            <form onSubmit={addUser}>
-            <input type='text' placeholder='Enter Username' value={uname}
-            onChange={(e)=>setUname(e.target.value)}/>
-             <input type='email' placeholder='Enter Email' value={email}
-            onChange={(e)=>setEmail(e.target.value)}/>
-            <button>Add User</button>
-            </form>
-        </div>
-    );
-}
-
-export default Demo2;
+  const [users,setUsers]= useState([])
+  useEffect(()=>{
+    axios
+    .get(URL)
+    .then((response) => response.data)
+    .then((data) => {
+    setUsers(data)
+    });
+  },[])
+  
+   
+    return <div>
+    {users.map((data)=>(
+      <div key={data.email}>
+       {data.name} --- {data.email}
+      </div>
+    ))}
+    </div>;
+  }
+export default RestApp
