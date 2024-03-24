f64976e32e9fc15ac1c3ff77bee2263824738bda
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-03-24 11:01:07 +0530

    Update data.js

--- data.js
+++ data.js
@@ -49,6 +49,28 @@ function NavBar(props) {
 export default NavBar;
 
 
+------
+
+
+   import logo from './logo.svg';
+import './App.css';
+ import { useRoutes } from 'react-router-dom'
+ import {appRoutes} from './routes'
+import LandingPage from './components/LandingPage';
+function App() {
+  const element= useRoutes(appRoutes)
+  return (
+    <div className="App">
+      <LandingPage/>
+      {element}
+    </div>
+  );
+}
+
+export default App;
+
+
+
 
 -------
 
