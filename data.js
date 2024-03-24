24b156d3dcf63a0424f03ea6c533728c6d3e1b5c
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-03-24 11:09:52 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,77 +1,27 @@
-import Login from "./components/Login";
-import LandingPage from "./components/LandingPage";
-import Register from "./components/Register";
-import Portfolio from "./components/Portfolio";
-
-
-export const appRoutes=[
-   
-    {path:'/login', element: <Login/>},
-    {path:'/register',element: <Register/>},
-    {path:'/portfolio',element: <Portfolio/>}
-]
-
----------
-
-
-    import React from 'react';
-import NavBar from './NavBar';
-
-function LandingPage(props) {
-    return (
-        <div>
-
-            <NavBar/>
-            <hr/>
-
-
-        </div>
-    );
-}
-
-export default LandingPage;
-
------
-
-    import React from 'react';
+import React from 'react';
 import {Link} from 'react-router-dom'
 
 function NavBar(props) {
     return (
-        <div>
-            
- <Link to="/login">Login</Link>
- <Link to="/register">Register</Link>
+        <div className='container'>
+
+
+            <ul class="nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">
+                        <Link to="/login">Login</Link>
+                    </a>
+
+                </li>
+                <li class="nav-item">
+
+                    <a class="nav-link active" aria-current="page" href="#">
+                        <Link to="/register">Register</Link>
+                    </a>
+                </li>
+            </ul>
         </div>
     );
 }
 
 export default NavBar;
-
-
-------
-
-
-   import logo from './logo.svg';
-import './App.css';
- import { useRoutes } from 'react-router-dom'
- import {appRoutes} from './routes'
-import LandingPage from './components/LandingPage';
-function App() {
-  const element= useRoutes(appRoutes)
-  return (
-    <div className="App">
-      <LandingPage/>
-      {element}
-    </div>
-  );
-}
-
-export default App;
-
-
-
-
--------
-
-    
