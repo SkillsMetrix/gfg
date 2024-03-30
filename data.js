a6d0fc4bfc8f5335cbf29cc1f14bd64593614269
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-03-30 11:33:06 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,86 +1,40 @@
-import React from 'react'
-import {Link,useNavigate} from 'react-router-dom'
 
-export default function NavBar() {
-   const token  = localStorage.getItem("token")
-   const navigate = useNavigate()
-    return (
-        <nav>
-        <div className="nav-wrapper #673ab7 deep-purple">
-          <Link to="/" className="brand-logo left">React GraphQL App for persistent</Link>
-          <ul id="nav-mobile" className="right">
-            {
-              token ?
-              <>
-               <li><Link to="/profile">Profile</Link></li>
-               <li><Link to="/create">Create</Link></li>
-               <li><button className="red btn" onClick={()=>{
-                 localStorage.removeItem("token")
-                 navigate('/login')
-               }}>Logout</button></li>
+import { combineReducers } from 'redux'
+import counterReducer from './counterReducer'
+const rootReducer= combineReducers({
+    counter:counterReducer
+})
 
-              </>:
-              <>
-               <li><Link to="/login">Login</Link></li>
-                <li><Link to="/signup">Signup</Link></li>
-               </>
-            }
-            
-           
-          </ul>
-        </div>
-      </nav>
-    )
+export default rootReducer
+
+
+
+
+const counterReducer=(state=10)=>{
+
+    return state
 }
-----
+export default counterReducer
 
 
 
-    import React, {useState, useEffect} from 'react';
-import { useNavigate } from 'react-router-dom';
 
-export default function Login() {
-    const [formData, setFormData] = useState({})
+   import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from './redux/reducers'
+import reportWebVitals from './reportWebVitals';
 
-    const Navigate = useNavigate();
-    const handleData = (e) => {
-        setFormData({
-            ...formData,
-            [e.target.name]: e.target.value
-        })
-        console.log(formData);
+const myStore=createStore(rootReducer)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={myStore}>
+    <App />
+  </Provider>
+);
 
-    }
-    const addUser = (e) => {
-        e.preventDefault()
-        const userData = localStorage.getItem('gfg')
-        const data = JSON.parse(userData)
-        console.log(data);
-        if(data.uname === formData.uname &&
-            data.email === formData.email){
-            Navigate('/Portfolio')
-        }
-        
-      
-    }
-
-  return (
-    <div>
-      <form onSubmit={addUser}>
-        <input
-          type="text"
-          placeholder="Enter Username"
-          name="uname"
-          onChange={handleData}
-        />
-        <input
-          type="email"
-          placeholder="Enter Email"
-          name="email"
-          onChange={handleData}
-        />
-        <button>Login</button>
-      </form>
-    </div>
-  );
-}
+ 
+reportWebVitals();
