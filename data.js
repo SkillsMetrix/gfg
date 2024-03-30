2aa8c174e4fc35e4ef072b3b0a9020a892b3499b
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-03-30 12:06:37 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,40 +1,15 @@
-
-import { combineReducers } from 'redux'
-import counterReducer from './counterReducer'
-const rootReducer= combineReducers({
-    counter:counterReducer
-})
-
-export default rootReducer
-
-
-
-
-const counterReducer=(state=10)=>{
-
-    return state
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux'
+import { increment } from '../redux/actions/counterAction';
+function CounterApp(props) {
+    const count= useSelector(state => state.counter)
+    const dispatch= useDispatch()
+    return (
+        <div>
+            <p>The count is : {count}</p>
+            <button onClick={()=> dispatch(increment())}>Increment</button>
+        </div>
+    );
 }
-export default counterReducer
 
-
-
-
-   import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import rootReducer from './redux/reducers'
-import reportWebVitals from './reportWebVitals';
-
-const myStore=createStore(rootReducer)
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Provider store={myStore}>
-    <App />
-  </Provider>
-);
-
- 
-reportWebVitals();
+export default CounterApp;
