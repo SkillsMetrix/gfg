423e88b00778ebbe48cdda722a8c12ae36710e31
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-04-13 11:32:38 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,48 +1,16 @@
-const jwt= require('jsonwebtoken')
+var request = require('request')
 
-const config= require('./config')
+var URL='https://api.openweathermap.org/data/2.5/weather?q=London&appid=b3aaa0b3323c0baab93aff38f75b44cb&units=metric'
 
-module.exports=(req,res,next) =>{
-
-    const token=req.header['x-access-token']
-    if(token) {
-        jwt.verify(token,config.secret,function(err,decoded){
-            if(err){
-                return res.status(401).json({"error":true,"message":"UnAuthorized Access"})
-            }
-            req.decoded= decoded
-            next()
-        })
+request({
+    url: URL,
+    json: true
+}, function(error,response,body){
+    if(error){
+        console.log('something went wrong');
+        
     }else {
-        res.status(403).send({"error":true,"message":"No Token Found"})
+        console.log('Its ' + body.main.temp + ' in ' + body.name);
+        
     }
-}
-
-
-
-
-
-
-
-const express= require('express')
-const json= require('jsonwebtoken')
-const router= express.Router()
-
-const config= require('./config')
-
-const app=express()
-const bp= require('body-parser')
-
-
-app.use(bp.json())
-
-router.use(require("./tokenChecker"))
-router.get("/secure",(req,res)=>{
-    res.send('private and secured route')
-})
-
-app.use("/api",router)
-app.listen(4000,()=>{
-console.log('server is ready');
-
 })
