f6e3b9da4997b397c38237d7ef4a7bcea289673c
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-04-13 11:56:57 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,16 +1,14 @@
-var request = require('request')
+var weather = require('./Weather.js')
+var argv= require('yargs')
+.option('location',{
+    description:'Location to get the weather for city',
+    demand:true,
+    type:'string'
+})
+.help('help')
+.argv
 
-var URL='https://api.openweathermap.org/data/2.5/weather?q=London&appid=b3aaa0b3323c0baab93aff38f75b44cb&units=metric'
+weather(argv.location,function (callback) {
+    console.log(callback);
 
-request({
-    url: URL,
-    json: true
-}, function(error,response,body){
-    if(error){
-        console.log('something went wrong');
-        
-    }else {
-        console.log('Its ' + body.main.temp + ' in ' + body.name);
-        
-    }
 })
