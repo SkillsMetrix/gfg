e294e2cc303d287dc5794ce2b57170c4d0683718
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-04-13 12:10:13 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,14 +1,37 @@
-var weather = require('./Weather.js')
+var request = require('request')
+
+
+module.exports= function(location,callback){
+ var URL=`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=b3aaa0b3323c0baab93aff38f75b44cb&units=metric`
+
+request({
+    url: URL,
+    json: true
+}, function(error,response,body){
+    if(error){
+        callback('something went wrong');
+        
+    }else {
+        callback('Its ' + body.main.temp + ' in ' + body.name);
+        
+    }
+})
+}
+
+-------
+
+    var weather = require('./Weather.js')
 var argv= require('yargs')
 .option('location',{
     description:'Location to get the weather for city',
     demand:true,
+    alias:'l',
     type:'string'
 })
 .help('help')
 .argv
 
-weather(argv.location,function (callback) {
+weather(argv.l,function (callback) {
     console.log(callback);
 
 })
