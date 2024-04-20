57b797ca4cef14fd935dbad99e7e248eeed2255f
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-04-20 12:06:30 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,3 +1,37 @@
-{ "Name" : "Balaji", "skills" : [ "Dancing", "Cooking", "Singing" ] }
-{ "Name" : "Ramesh", "skills" : [ "Cooking", "Singing" ] }
-{ "Name" : "Suresh", "skills" : [ "Dancing", "Singing" ] }
+
+var express= require('express')
+var bp= require('body-parser')
+var mongoose= require('mongoose')
+const empcrud= require('./model')
+var app= express()
+app.use(bp.json())
+
+
+
+app.post('/adduser',(req,res)=>{
+    const users= new empcrud({
+        ...req.body
+    })
+    users.save().then(()=> res.send('user is added'))
+})
+app.get("/loadusers",async(req,res)=>{
+    const users= await empcrud.find()
+    res.status(200).json(users)
+})
+app.get("/loadusers/:uname",async(req,res)=>{
+    const name=req.params.uname
+    const users= await empcrud.find({'uname':name})
+    res.status(200).json(users)
+})
+const startServer=async()=>{
+
+    await mongoose.connect('mongodb+srv://amar:amar123@cluster0.rle5i.mongodb.net/gfg?retryWrites=true&w=majority&appName=Cluster0')
+    app.listen(4000,()=>{
+        console.log('server is ready with DB connected');
+        
+    })
+}
+
+startServer()
+
+
