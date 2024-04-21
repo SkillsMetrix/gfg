f7bf8326fedea0572103097defb490d4dfa3dbed
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-04-21 11:15:36 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,37 +1,42 @@
+import {ActionTypes} from "../constants/action-types"
 
-var express= require('express')
-var bp= require('body-parser')
-var mongoose= require('mongoose')
-const empcrud= require('./model')
-var app= express()
-app.use(bp.json())
-
-
-
-app.post('/adduser',(req,res)=>{
-    const users= new empcrud({
-        ...req.body
-    })
-    users.save().then(()=> res.send('user is added'))
-})
-app.get("/loadusers",async(req,res)=>{
-    const users= await empcrud.find()
-    res.status(200).json(users)
-})
-app.get("/loadusers/:uname",async(req,res)=>{
-    const name=req.params.uname
-    const users= await empcrud.find({'uname':name})
-    res.status(200).json(users)
-})
-const startServer=async()=>{
-
-    await mongoose.connect('mongodb+srv://amar:amar123@cluster0.rle5i.mongodb.net/gfg?retryWrites=true&w=majority&appName=Cluster0')
-    app.listen(4000,()=>{
-        console.log('server is ready with DB connected');
-        
-    })
+const initialState = {
+    products: [
+        {
+            id: 101,
+            productName: 'Watch',
+            category: 'Electronics'
+        }
+    ]
+}
+export const productReducer = (state = initialState, {type, payload}) => {
+    switch (type) {
+        case ActionTypes.SET_PRODUCTS:
+            return state
+        default:
+            return state
+    }
 }
 
-startServer()
+
+-----
 
 
+
+    import { ActionTypes } from "../constants/action-types"
+
+export const setProducts=(products)=>{
+    return {
+    type:ActionTypes.SET_PRODUCTS,
+    payload:products
+    }
+}
+
+
+
+export const selectedProduct=(product)=>{
+    return {
+    type:ActionTypes.SELECTED_PRODUCTS,
+    payload:product
+    }
+}
