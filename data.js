97464229b48b35557148655bf5ab3f5030bef03f
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-04-21 11:36:52 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,3 +1,34 @@
+----constants-----
+
+export const ActionTypes={
+    SET_PRODUCTS:"SET_PRODUCTS",
+    SELECTED_PRODUCTS:"SELECTED_PRODUCTS",
+    
+}
+
+-----Actions------
+import { ActionTypes } from "../constants/action-types"
+
+export const setProducts=(products)=>{
+    return {
+    type:ActionTypes.SET_PRODUCTS,
+    payload:products
+    }
+}
+
+
+
+export const selectedProduct=(product)=>{
+    return {
+    type:ActionTypes.SELECTED_PRODUCTS,
+    payload:product
+    }
+}
+-----
+
+    -----reducers--------
+
+
 import {ActionTypes} from "../constants/action-types"
 
 const initialState = {
@@ -18,25 +49,56 @@ export const productReducer = (state = initialState, {type, payload}) => {
     }
 }
 
+-----
+
+    
+import { combineReducers } from 'redux'
+import { productReducer } from './productReducer'
+ 
+const rootReducer=combineReducers({
+    allProducts:productReducer
+})
+
+
+export default rootReducer
+
+
 
 -----
 
 
 
-    import { ActionTypes } from "../constants/action-types"
-
-export const setProducts=(products)=>{
-    return {
-    type:ActionTypes.SET_PRODUCTS,
-    payload:products
-    }
-}
+    import { createStore } from 'redux'
+ import rootReducer from './reducers/index'
 
 
 
-export const selectedProduct=(product)=>{
-    return {
-    type:ActionTypes.SELECTED_PRODUCTS,
-    payload:product
-    }
-}
+const store= createStore(rootReducer)
+
+
+export default store
+
+
+-----
+
+
+    import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+ import {Provider} from 'react-redux'
+import appStore from './redux/store'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={appStore}>
+    <App />
+  </Provider>
+);
+
+ 
+reportWebVitals();
+
+
+
