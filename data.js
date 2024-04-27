670fd5e29708ce0e6413e924c98e047d84a6e575
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-04-27 11:52:05 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,78 +1,43 @@
-import {ActionTypes} from "../constants/action-types"
-
-const initialState = {
-    products: []
-}
-export const productReducer = (state = initialState, {type, payload}) => {
-    switch (type) {
-        case ActionTypes.SET_PRODUCTS:
-            return {...state,products:payload}
-        default:
-            return state
-    }
-}
-
----
-
-
-    import React, {useEffect} from 'react';
-import axios from 'axios'
-import {useSelector, useDispatch} from 'react-redux'
-import ProductComponent from './ProductComponent';
-import { setProducts } from '../redux/actions/productActions';
-
-function ProductListing(props) {
-    const data = useSelector(state => state.allProducts)
-    const dispatch=useDispatch()
-
-    const loadProducts = async () => {
-        const response = await axios.get('https://fakestoreapi.com/products')
-         dispatch(setProducts(response.data))
-}
-    useEffect(() => {
-        loadProducts()
-    }, [])
-console.log(data);
-
-    return (
-        <div className="ui grid container">
-            <h3>Product Listing</h3>
-            <ProductComponent/>
-        </div>
-    );
-}
-
-export default ProductListing;
-
-
-
-------
-
-
-
-    import React from 'react';
+import React from 'react';
 import {useSelector} from 'react-redux'
+import { Link } from 'react-router-dom'
 function ProductComponent(props) {
 
 
-    const data = useSelector(state => state.allProducts.products)
-    //const {id, productName, category} = data[0]
-    return (
-        <div className="four wide column">
-            <div className="ui link cards">
-                <div className="card">
-                    <div className="image"></div>
-                    <div className="content">
-                        <div className="header">
-                            </div>
+    const products = useSelector(state => state.allProducts.products)
+    const renderList = products.map((product) => {
+        const {
+            id,
+            title,
+            image,
+            price,
+            category
+        } = product
+        return (
 
+            <div className="four wide column">
+                <Link to={`/productdetails/${id}`}>
+                <div className="ui link cards">
+                    <div className="card">
+                        <div className="image">
+                            <img src={image}/>
+                        </div>
+                        <div className="content">
+                            <div className="header">
+                                {title}</div>
+
+                            <div className="meta price ">4{price}</div>
+                            <div className="meta">
+                                {category}</div>
+                        </div>
                     </div>
                 </div>
+                </Link>
+
             </div>
-
-
-        </div>
-    );
+        );
+    })
+    return <>{renderList}</>
 }
 
 export default ProductComponent;
