26ee23885b212a59975b63f3f1181bb1d731d1f5
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-05-04 12:23:26 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,6 +1,7 @@
 
 import { render, screen } from '@testing-library/react';
-import Login from './Login';
+import Login, { validateEmail } from './Login';
+import userEvent from '@testing-library/user-event'
 
 describe('Test the suite',()=>{
     test('render the button',async()=>{
@@ -9,4 +10,35 @@ describe('Test the suite',()=>{
          expect(buttonList).toHaveLength(2)
 
     })
+    test('check email value',()=>{
+        const email='admin@mail.com'
+        expect(validateEmail(email)).toBe(true)
+    })
+    test('check value',()=>{
+        render(<Login/>)
+        const email=screen.getByPlaceholderText('Enter email')
+        userEvent.type(email,"admin@mail.com")
+        expect(email.value).toMatch('admin@mail.com')
+    })
+
+    test('check password',()=>{
+        render(<Login/>)
+        const password= screen.getByPlaceholderText('Password')
+
+        
+        expect(password).toHaveAttribute("type","password")
+    })
+    test('value chack',async()=>{
+
+        render(<Login/>)
+        const email=screen.getByPlaceholderText('Enter email')
+        const password= screen.getByPlaceholderText('Password')
+        const buttonList= await screen.findAllByRole('button')
+        userEvent.type(email,"adminmailcom")
+        userEvent.type(password,"1234")
+        userEvent.click(buttonList[0])
+        const error= screen.getByText("Email is not valid")
+        expect(error).toBeInTheDocument()
+    })
+
 })
