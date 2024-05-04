e88081ceded30338055586b9cac4805591c7cefb
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-05-04 11:14:15 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,132 +1,86 @@
-import {ActionTypes} from "../constants/action-types"
+import React, { useState } from "react";
+import { Form, Button, Alert } from "react-bootstrap";
 
-const initialState = {
-    products: []
-}
-export const productReducer = (state = initialState, {type, payload}) => {
-    switch (type) {
-        case ActionTypes.SET_PRODUCTS:
-            return {...state,products:payload}
-            case ActionTypes.FETCH_PRODUCTS:
-                return {...state,products:payload}
-        default:
-            return state
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [showUser, setShowUser] = useState(false);
+
+  const handleSubmit = (e) => {
+    setShowUser(false);
+    e.preventDefault();
+    if (validateEmail(email)) {
+      setShowUser(true);
+      setError("");
+      return;
     }
-}
+    setError("Email is not valid");
+    return false;
+  };
 
-export const selectedProduct = (state ={}, {type, payload}) => {
-    switch (type) {
-        case ActionTypes.SELECTED_PRODUCTS:
-            return {...state,...payload}
-        default:
-            return state
-    }
-}
+  const resetForm = () => {
+    setEmail("");
+    setPassword("");
+    setShowUser(false);
+  };
 
+  return (
+    <>
+      <h2>We will Test the Login Form Component</h2>
+      {showUser && (
+        <Alert data-testid="user" variant="success">
+          {email}
+        </Alert>
+      )}
+      {error && (
+        <Alert data-testid="error" variant="danger">
+          {error}
+        </Alert>
+      )}
+      <Form onSubmit={handleSubmit} noValidate>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
 
------
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
 
+        <Button data-testid="submit" variant="primary" type="submit">
+          Submit
+        </Button>
+        <Button
+          variant="secondary"
+          data-testid="reset"
+          onClick={resetForm}
+          style={{ marginLeft: "5px" }}
+        >
+          Reset
+        </Button>
+      </Form>
+    </>
+  );
+};
 
-    import { ActionTypes } from "../constants/action-types"
-import axios from "axios"
-import fakeStore from "../../apis/fakeStore"
+export const validateEmail = (email) => {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+  if (regex.test(email)) {
+    return true;
+  }
+  return false;
+};
 
-
-
-
-export const fetchProducts=()=>async(dispatch)=>{
-      const response=await fakeStore.get('/products')
-     dispatch({type:ActionTypes.FETCH_PRODUCTS,payload:response.data})
-     
-}
-export const fetchProduct=(id)=>async(dispatch)=>{
-    const response=await fakeStore.get(`/products/${id}`)
-   dispatch({type:ActionTypes.FETCH_PRODUCTS,payload:response.data})
-   
-}
-
-export const setProducts=(products)=>{
-    return {
-    type:ActionTypes.SET_PRODUCTS,
-    payload:products
-    }
-}
-
-
-
-export const selectedProduct=(product)=>{
-    return {
-    type:ActionTypes.SELECTED_PRODUCTS,
-    payload:product
-    }
-}
-
-
-
-
------
-
-    import React, {useEffect} from 'react';
-import axios from 'axios'
-import {useSelector, useDispatch} from 'react-redux'
-import {useParams} from 'react-router-dom'
-import {selectedProduct, fetchProduct} from '../redux/actions/productActions';
-
-function ProductDetails(props) {
-    const product = useSelector((state) => state.product)
-    const {
-        title,
-        price,
-        category,
-        image,
-        description
-    } = product
-    const dispatch = useDispatch()
-    const {productid} = useParams()
-
-    
-    useEffect(() => {
-       dispatch(fetchProduct(productid))
-    }, [])
-
-
-    return (
-        <div className="ui grid container">
-            <div className="ui placeholder segment">
-                <div className="ui two column stackable center aligned grid">
-                    <div className="ui vertical divider">AND</div>
-                    <div className="middle aligned row">
-                        <div className="column lp">
-                            <img className="ui fluid image"
-                                src={image}/>
-                        </div>
-                        <div className="column rp">
-                            <h1>{title}</h1>
-                            <h2>
-                                <a className="ui teal tag label">${price}</a>
-                            </h2>
-                            <h3 className="ui brown block header">
-                                {category}</h3>
-                            <p>{description}</p>
-                            <div className="ui vertical animated button" tabIndex="0">
-                                <div className="hidden content">
-                                    <i className="shop icon"></i>
-                                </div>
-                                <div className="visible content">Add To Cart</div>
-                            </div>
-
-                        </div>
-
-
-                    </div>
-
-
-                </div>
-
-            </div>
-
-        </div>
-    );
-}
-
-export default ProductDetails;
+export default Login;
